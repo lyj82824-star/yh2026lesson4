e10b8a4521838b1fc92dba905d9fd61dd5e3a606
lyj82824-star <lyj82824@goedu.kr>
2026-08-12 16:44:34 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,13 @@
-import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
 
-export default defineConfig(() => {
-  return {
-    base: '/yh2026lesson4/',
-    plugins: [react(), tailwindcss()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  base: '/yh2026lesson4/',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-  };
+  },
 });
